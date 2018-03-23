06605197f7c411a4d83594f0000743cbaa45fac7
Jose Javier Romero Rivas <romero@strands.com>
2018-03-23 16:44:47 +0100

    - Adding react-router-dom to be able to work with url parameters later

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import globalReducer from "./reducers/index.js"
+import globalReducer from "./reducers/index.js";
+import { BrowserRouter as Router,Route } from "react-router-dom";
 
 let store = createStore(globalReducer);
 
 ReactDOM.render(
     <Provider store={store}>
-        <div>
-            <App/>
-        </div>
+        <Router>
+            <Route path="/" component={App}/>
+        </Router>
     </Provider>,
     document.getElementById("app")
 );
